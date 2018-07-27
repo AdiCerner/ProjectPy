b4b78b6594531eaab419b5eba6879f2d77413ef3
adicerner <adianil3492@gmail.com>
2018-07-27 10:16:10 +0530

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,3 @@
+# This is my 1st Py program
+
+print("Hi \n Wel Come to this python world")
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\python\PyC\Basics\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
